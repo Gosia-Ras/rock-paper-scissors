1a3cbdeadc17401bd3c2d6da51c72b5840bf96f0
Gosia Hildebrand <ras.malgorzata@gmail.com>
2023-11-09 18:11:41 +0100

    Initial functionality of the game, result prints to the console

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
-console.log("Hello world");
+const choices = ["rock", "paper", "scissors"];
+
+function getComputerChoice(array) {
+  const randomIndex = Math.floor(Math.random() * array.length);
+  const item = array[randomIndex];
+  return item;
+}
+
+const playerSelection = "rock";
+const computerSelection = getComputerChoice(choices);
+
+function playRound(playerSelection, computerSelection) {
+  if (playerSelection === "rock" && computerSelection === "paper") {
+    console.log("Sorry, you lost!");
+  }
+  if (playerSelection === "rock" && computerSelection === "scissors") {
+    console.log("You've won!");
+  }
+  if (playerSelection === computerSelection) {
+    console.log("It's a tie!");
+  }
+}
+
+playRound(playerSelection, computerSelection);
